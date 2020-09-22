8a0e5336bec3fd09d36d51c2d33c558cb2acc098
Pez Cuckow <email@pezcuckow.com>
2020-09-22 11:37:56 +0100

    ✨ Change the icon on click to confirm sorting done

--- ext/js/background.js
+++ ext/js/background.js
@@ -14,9 +14,19 @@ function checkForValidUrl(tabId, changeInfo, tab) {
       title: 'Sorting not available on this page',
     })
   }
+
+  chrome.pageAction.setIcon({
+    tabId: tab.id,
+    path: 'icons/icon19.png',
+  })
 }
 chrome.tabs.onUpdated.addListener(checkForValidUrl)
 
 chrome.pageAction.onClicked.addListener((tab) => {
   chrome.tabs.executeScript(tab.id, { file: 'js/sort.js' })
+
+  chrome.pageAction.setIcon({
+    tabId: tab.id,
+    path: 'icons/icon19-checked.png',
+  })
 })
