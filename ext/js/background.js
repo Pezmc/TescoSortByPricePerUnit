bab8751679023d89c7cf0928ce56436931a59fd2
Pez Cuckow <email@pezcuckow.com>
2020-09-22 11:37:34 +0100

    ✨ Use the page_actions api to enable and disable the button

--- ext/js/background.js
+++ ext/js/background.js
@@ -1,4 +1,22 @@
-chrome.browserAction.onClicked.addListener((_tabs) => {
-  console.log('RUNNING')
-  chrome.tabs.executeScript(null, { file: 'js/sort.js' })
+/* global chrome */
+
+function checkForValidUrl(tabId, changeInfo, tab) {
+  if (tab.url?.includes('tesco.com/groceries')) {
+    chrome.pageAction.show(tabId)
+    chrome.pageAction.setTitle({
+      tabId,
+      title: 'Sort page by Price Per Unit',
+    })
+  } else {
+    chrome.pageAction.hide(tabId)
+    chrome.pageAction.setTitle({
+      tabId,
+      title: 'Sorting not available on this page',
+    })
+  }
+}
+chrome.tabs.onUpdated.addListener(checkForValidUrl)
+
+chrome.pageAction.onClicked.addListener((tab) => {
+  chrome.tabs.executeScript(tab.id, { file: 'js/sort.js' })
 })
