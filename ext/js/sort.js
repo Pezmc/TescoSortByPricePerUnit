008db74248d567b833397286d3ff00bb614e3660
Pez Cuckow <email@pezcuckow.com>
2022-06-12 10:02:24 +0100

    :sparkles: Handle the 2022 classes

--- ext/js/sort.js
+++ ext/js/sort.js
@@ -1,42 +1,59 @@
-(function () {
+window.sortTesco = function () {
   const knownUnits = [
-    '/1g',
-    '/10g',
-    '/100g',
-    '/kg',
-    '/1ml',
-    '/10ml',
-    '/100ml',
-    '/litre',
-    '/each',
+    '1g',
+    '10g',
+    '100g',
+    'kg',
+    '1ml',
+    '10ml',
+    '100ml',
+    'litre',
+
+    // not normalised
+    'each',
+    'sht',
+    '10sht',
+    '100sht',
+    '1000sht',
     'none',
   ]
 
   function normalize(price, unit, name) {
+    unit = unit.replace('/', '')
+
     if (!knownUnits.includes(unit)) {
       console.log(`Unknown unit "${unit}"`)
     }
 
     // todo: Make this more generic
-    if (unit == '/1g' || unit == '/1ml') {
+    if (unit == '1g' || unit == '1ml') {
       price = price * 1000
-      unit = unit == '/1g' ? '/kg' : '/litre'
-    } else if (unit == '/10g' || unit == '/10ml') {
+      unit = unit == '1g' ? 'kg' : 'litre'
+    } else if (unit == '10g' || unit == '10ml') {
       price = price * 100
-      unit = unit == '/10g' ? '/kg' : '/litre'
-    } else if (unit == '/100g' || unit == '/100ml') {
+      unit = unit == '10g' ? 'kg' : 'litre'
+    } else if (unit == '100g' || unit == '100ml') {
       price = price * 10
-      unit = unit == '/100g' ? '/kg' : '/litre'
+      unit = unit == '100g' ? 'kg' : 'litre'
     }
 
     return [price, unit, name]
   }
 
   function grabProductInfo(productNode) {
-    const infoNode = productNode.querySelector('.price-per-quantity-weight')
+    const infoNode = productNode.querySelector(
+      '.price-per-quantity-weight, [class*=-price__subtext]',
+    )
     const priceNode = infoNode?.querySelector('[data-auto="price-value"]')
-    const price = parseFloat(priceNode?.textContent.trim() || 0)
-    const unit = infoNode?.querySelector('.weight').textContent.trim() || 'none'
+    const priceStr =
+      priceNode?.textContent.trim() ||
+      infoNode?.textContent.trim().split('/')?.[0]
+    const price = parseFloat(priceStr?.replace(/[^0-9.-]+/g, '') || 0)
+
+    const unit =
+      infoNode?.querySelector('.weight')?.textContent.trim() ||
+      infoNode?.textContent.trim().split('/')?.[1] ||
+      'none'
     const nameNode = productNode?.querySelector(
       '[data-auto="product-tile--title"]',
     )
@@ -52,7 +69,17 @@
     const [bPrice, bUnit, bName] = grabProductInfo(b)
 
     if (!knownUnits.includes(bUnit)) {
-      console.log('Unknown unit', bUnit)
+      console.warn('Unknown unit', bUnit)
+    }
+
+    if (!aPrice) {
+      console.info('No price found for', aName)
+      //console.debug(a)
+    }
+
+    if (!bPrice) {
+      console.info('No price found for', bName)
+      //console.debug(b)
     }
 
     // Sort by price
@@ -82,4 +109,6 @@
   })
 
   console.log('Sorting complete!')
-})()
+}
+
+window.sortTesco()
--- test/sort.test.js
+++ test/sort.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const { describe, expect, test } = require('@jest/globals')
 
 test('sorts the products by price per unit', () => {
@@ -61,6 +65,128 @@ test('sorts the products by price per unit', () => {
     '£0.13/10ml',
     '£0.05/1ml',
     '£0.15/1ml',
+  ]
+
+  expect(prices).toEqual(expected)
+})
+
+test('sorts the 2022 products by price per unit with single units', () => {
+  document.body.innerHTML = require('./groceries2022.html')
+
+  window.sortTesco()
+
+  const priceElements = document.body.querySelectorAll(
+    '[data-auto="product-list"] [class*=price__subtext]',
+  )
+
+  const prices = Array.from(priceElements).map((el) => {
+    return el.textContent.trim().replace(/\s*/g, '')
+  })
+
+  const expected = [
+    '£2.78/kg',
+    '£4.02/kg',
+    '£4.69/kg',
+    '£5.00/kg',
+    '£5.00/kg',
+    '£5.00/kg',
+    '£5.00/kg',
+    '£6.24/kg',
+    '£6.88/kg',
+    '£6.88/kg',
+    '£7.34/kg',
+    '£7.50/kg',
+    '£7.50/kg',
+    '£7.50/kg',
+    '£7.50/kg',
+    '£7.71/kg',
+    '£7.71/kg',
+    '£7.71/kg',
+    '£8.66/kg',
+    '£10.00/kg',
+    '£10.00/kg',
+    '£10.26/kg',
+    '£11.25/kg',
+    '£11.46/kg',
+    '£11.46/kg',
+    '£11.54/kg',
+    '£12.50/kg',
+    '£12.50/kg',
+    '£12.50/kg',
+    '£13.24/kg',
+    '£15.00/kg',
+    '£16.55/kg',
+    '£17.65/kg',
+    '£22.03/kg',
+    '£25.43/kg',
+    '£28.58/kg',
+  ]
+
+  expect(prices).toEqual(expected)
+})
+
+test('sorts the 2022 products by price per unit with mixed and unsorted units', () => {
+  document.body.innerHTML = require('./groceries2022-mixed.html')
+
+  window.sortTesco()
+
+  const priceElements = document.body.querySelectorAll(
+    '[data-auto="product-list"] [class*=price__subtext]',
+  )
+
+  const prices = Array.from(priceElements).map((el) => {
+    return el.textContent.trim().replace(/\s*/g, '')
+  })
+
+  const expected = [
+'£1.00/100sht',
+    '£1.17/100sht',
+    '£1.17/100sht',
+    '£1.17/100sht',
+    '£1.43/100sht',
+    '£1.75/100sht',
+    '£1.75/100sht',
+    '£1.93/100sht',
+    '£1.96/100sht',
+    '£2.09/100sht',
+    '£3.75/100sht',
+    '£5.00/100sht',
+    '£0.00/each',
+    '£0.00/each',
+    '£0.03/each',
+    '£0.05/each',
+    '£0.10/each',
+    '£0.12/each',
+    '£0.13/each',
+    '£0.14/each',
+    '£0.64/100g',
+    '£0.64/100g',
+    '£0.86/100g',
+    '£1.00/100g',
+    '£1.00/100g',
+    '£1.15/100g',
+    '£11.88/kg',
+    '£1.29/100g',
+    '£1.39/100g',
+    '£1.39/100g',
+    '£1.39/100g',
+    '£1.51/100g',
+    '£15.63/kg',
+    '£4.59/100g',
+    '£5.53/100g',
+    '£5.53/100g',
+    '£5.84/100g',
+    '£5.84/100g',
+    '£11.67/100g',
+    '£11.67/100g',
+    '£1.50/litre',
+    '£0.20/100ml',
+    '£3.75/litre',
+    '£4.50/litre',
+    '£4.50/litre',
+    '£4.75/litre',
+    '£5.00/litre',
+    '£3.50/100ml',
   ]
 
   expect(prices).toEqual(expected)
